294bdf10867b11b504d1b9f9a38d1003cc22bf96
projectblackiron <razakadam74@gmail.com>
2021-12-27 19:33:07 -0800

    added pages to projects

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,24 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 import { AppRoutes } from './AppRoutes';
+import ForgetPasswordPage from './pages/ForgetPasswordPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LogInPage';
+import DashboardPage from './pages/secured/Dashboard';
+import SignUpPage from './pages/SignUpPage';
 import { RequireAuth } from './providers/AuthProvider';
-
-function getTitle(title: string): JSX.Element {
-  return (
-    <div>
-      <p>{title}</p>
-    </div>
-  );
-}
 
 export function App() {
   return (
     <Routes>
-      <Route path={AppRoutes.home} element={getTitle('home')} />
-      <Route path={AppRoutes.signUp} element={getTitle('signUp')} />
-      <Route path={AppRoutes.login} element={getTitle('login')} />
+      <Route path={AppRoutes.home} element={<HomePage />} />
+      <Route path={AppRoutes.signUp} element={<SignUpPage />} />
+      <Route path={AppRoutes.login} element={<LoginPage />} />
+      <Route path={AppRoutes.forgetPassword} element={<ForgetPasswordPage />} />
       <Route
         path={AppRoutes.dashboard}
-        element={<RequireAuth>{getTitle('dashboard')}</RequireAuth>}
+        element={<RequireAuth>{<DashboardPage />}</RequireAuth>}
       />
     </Routes>
   );
---
+++ apps/frontend/src/app/pages/secured/Dashboard.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react';
+
+const DashboardPage: FunctionComponent = () => {
+  return <p>{'dashboard'}</p>;
+};
+
+export default DashboardPage;
